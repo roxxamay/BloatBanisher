5cc0177150c3bcd11e9d7eff8f2fcfc19e92a083
roxxamay <itsmeadityakumararya@gmail.com>
2024-01-13 21:28:17 +0530

    GOOGLE debloat added with extra apps

--- BloatBanisher.sh
+++ BloatBanisher.sh
@@ -65,7 +65,52 @@ while [[ "$OPTION" != 5 ]]; do
 
     4)
      echo "Uninstalling GOOGLE Bloat"
-     adb uninstall --user 0 com.
-     ;;
+     adb uninstall --user 0 com.google.android.music  #google music
+     adb uninstall --user 0 com.google.android.apps.youtube.music.setupwizard  #google youtube system music
+     adb uninstall --user 0 com.google.android.apps.subscriptions.red    #google red AD
+     adb uninstall --user 0 com.google.android.videos   #google play videos
+     adb uninstall --user 0 com.google.android.apps.photos     #google photos
+     adb uninstall --user 0 com.android.android.apps.maps   # google maps
+     adb uninstall --user 0 com.google.android.apps.tachyon   #google duo / meets
+     adb uninstall --user 0 com.google.android.apps.nbu.files  #google files
+     adb uninstall --user 0 com.google.android.apps.docs    #google drive
+     adb uninstall --user 0 com.google.android.apps.docs.editors.sheets #google sheets
+     adb uninstall --user 0 com.google.android.apps.docs.editors.sheets  #google slides
+     adb uninstall --user 0 com.google.chrome   #google chrome
+     adb uninstall --user 0 com.google.android.gm    #google gmail
+     adb uninstall --user 0 com.google.android.googlequicksearchbox   #google app
+     adb uninstall --user 0 com.google.android.calendar    # google calendar
+     #adb uninstall --user 0 com.google.android.apps.messaging   #google message
+     adb uninstall --user 0 com.google.android.apps.wellbeing   # digital wellbeing
+     adb uninstall --user 0 com.google.android.apps.podcasts    # google podcasts
+     adb uninstall --user 0 com.google.android.apps.magazines    # google news
+     adb uninstall --user 0 com.google.android.apps.walletnfcrel   #google wallet
+     adb uninstall --user 0 com.google.ar.core   #play service for AR
+     adb uninstall --user 0 com.google.android.projection.gearhead   # andoid auto
+     adb uninstall --user 0 com.google.android.calculator  # google calculator
+     adb uninstall --user 0 com.google.android.apps.photosgo  # google gallery
+     adb uninstall --user 0 com.google.android.apps.chromecast.app  # google home
+     adb uninstall --user 0 com.google.android.apps.fitness   # google fit
+     adb uninstall --user 0 com.google.ar.lens  # google lens
+     adb uninstall --user 0 com.google.android.keep  # google keeps
+     adb uninstall --user 0 com.google.android.apps.nbu.paisa.user   # google pay
+     adb uninstall --user 0 com.google.android.videos    # google TV
+     adb uninstall --user 0 com.google.android.apps.safetyhub   # google safety
+     adb uninstall --user 0 com.google.android.apps.recorder   # google recorder
+     adb uninstall --user 0 com.google.android.youtube   # google youtube
+     adb uninstall --user 0 com.google.android.wearable.weather # google weather
+     adb uninstall --user 0 com.google.android.apps.youtube.kids  # youtube kids
+
+     echo "IF YOU WANT ANY APPS THAT ARE DELETED YOU CAN FURTHER INSTALL IT FROM PLAYSTORE "
+     echo "HERE IS THE LINK OF THE APPS >> https://play.google.com/store/apps/dev?id=5700313618786177705&hl=en_IN&gl=US&pli=1"
+     echo "YOU CAN FURTHER DOWNLOAD ADVANCE YOUTUBE ALTERNATIVE LIKE YOUTUBE VANCED , RVX "
+     echo "HERE IS THE LINK OF THE REPO OF THE APPS "
+     echo "RVX : https://github.com/NoName-exe/revanced-extended"
+     echo "MICROG : https://github.com/inotia00/VancedMicroG/releases"
+     echo "if you are root user download zip file and flash in magisk"
+     echo "if not install microg first then run youtube revanced extended"
+     #rvx is the project of >>> https://github.com/NoName-exe
+     #microg is the project of >>> https://github.com/inotia00
+    ;;
  esac
 done
